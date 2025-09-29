0dacf2dfe0e37331bd0ce8198e27aadefa72af92
ProductionOption <prod4option@gmail.com>
2025-09-29 12:31:50 -0500

    Yay! First program worked
    Yay! First program worked

--- 01_basic_input_output/01_print_c_programming.c
+++ 01_basic_input_output/01_print_c_programming.c
@@ -1,16 +1,24 @@
 // // C program to print C Programming on screen
 
 // // Header files
+// somewhere in the program
+
 #include <stdio.h>
 #include <conio.h>
 
 // // Main Function Start
 int main()
 {
-
+    printf("\n \n");
     printf("C Programming"); // // printf() function is used to print Output on screen
 
     getch();
     return 0;
 }
+
+
+
+
 // // Main Function End
+
+// This works using Visual Studio Code 
